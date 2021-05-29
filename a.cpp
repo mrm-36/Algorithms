d2e5fd3dab73dacc31705f2e644d2c0fdbc2ca7c
Miguel Merheb <74906227+mrm-36@users.noreply.github.com>
2021-05-29 13:52:00 +0300

    API Deleted a.cpp

--- a.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main(){
-    int64_t n, m, a; std::cin >> n >> m >> a;
-    std::cout << ((n / a + (n % a == 0 ? 0 : 1))
-                * (m / a + (m % a == 0 ? 0 : 1)));
-    return 0;
-}
