83ef0899937896aef645ab0939135f292a84dfab
Miguel Merheb <74906227+mrm-36@users.noreply.github.com>
2021-05-29 14:57:13 +0300

    API Created gcd-lcm.cpp

---
+++ gcd-lcm.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+// Complexity: O(log(min(a, b)))
+int64_t gcd(int64_t a, int64_t b) {return b == 0 ? a : gcd(b, a % b);}
+int64_t lcm(int64_t a, int64_t b) {return a / gcd(a, b) * b;}
